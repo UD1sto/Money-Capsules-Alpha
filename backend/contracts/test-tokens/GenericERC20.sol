ea133b7bdbbcf9b5746a92915c60a01ed7d34153
Harendra Shakya <harendrashakya2004@gmail.com>
2022-09-27 14:14:05 +0530

    improved generic erc20

--- backend/contracts/test-tokens/GenericERC20.sol
+++ backend/contracts/test-tokens/GenericERC20.sol
@@ -4,17 +4,19 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-uint8 dec = 18;
- 
 contract GenericERC20 is ERC20 {
-    constructor(string name, string symbol, uint _dec) ERC20("name", "symbol") {
-        _mint(msg.sender, 1000000000 * 10 ** decimals());
-        if (dec != 0){
-        dec = _dec
-        }
+    uint8 private dec = 18;
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 _dec
+    ) ERC20(name, symbol) {
+        dec = _dec;
+        _mint(msg.sender, 10000000000000 * 10**_dec);
     }
-    
-    function decimals() public view override returns (uint8) {
-        return dec;
+
+    function decimals() public view override returns (uint8 _dec) {
+        _dec = dec;
     }
 }
