3631d796b6a35d2b9fbf8e1372835c20f4eb2e7e
Harendra Shakya <harendrashakya2004@gmail.com>
2022-09-28 14:06:44 +0530

    added events and changed function names

--- backend/contracts/CapsuleFactory.sol
+++ backend/contracts/CapsuleFactory.sol
@@ -7,35 +7,38 @@ import "./MixedCapsule.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract CapsuleFactory {
-    address immutable walletImplementation;
-    address immutable nftWalletImplementation;
-    address immutable walletMixedImplementation;
-    event contractCreation(address cloneAddress, address ownedBy);
+    address private immutable erc20WalletImplementation;
+    address private immutable nftWalletImplementation;
+    address private immutable mixedWalletImplementation;
 
-    constructor() public {
-        walletImplementation = address(new ERC20Capsule());
+    event erc20CapsuleCreation(address cloneAddress, address ownedBy);
+    event nftCapsuleCreation(address cloneAddress, address ownedBy);
+    event mixedCapsuleCreation(address cloneAddress, address ownedBy);
+
+    constructor() {
+        erc20WalletImplementation = address(new ERC20Capsule());
         nftWalletImplementation = address(new NftCapsule());
-        walletMixedImplementation = address(new MixedCapsule());
+        mixedWalletImplementation = address(new MixedCapsule());
     }
 
-    function createWalletContract() public {
-        address clone = Clones.clone(walletImplementation);
+    function createERC20Capsule() external {
+        address clone = Clones.clone(erc20WalletImplementation);
         ERC20Capsule(clone).initialize();
         ERC20Capsule(clone).transferOwnership(msg.sender);
-        emit contractCreation(clone, msg.sender);
+        emit erc20CapsuleCreation(clone, msg.sender);
     }
 
-    function createWalletContract2() public {
+    function createNftCapsule() external {
         address clone = Clones.clone(nftWalletImplementation);
         NftCapsule(clone).initialize();
         NftCapsule(clone).transferOwnership(msg.sender);
-        emit contractCreation(clone, msg.sender);
+        emit nftCapsuleCreation(clone, msg.sender);
     }
 
-    function createWalletContract3() public {
-        address clone = Clones.clone(walletMixedImplementation);
+    function createMixedCapsule() external {
+        address clone = Clones.clone(mixedWalletImplementation);
         MixedCapsule(clone).initialize();
         MixedCapsule(clone).transferOwnership(msg.sender);
-        emit contractCreation(clone, msg.sender);
+        emit mixedCapsuleCreation(clone, msg.sender);
     }
 }
